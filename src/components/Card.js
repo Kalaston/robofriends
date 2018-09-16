63afe77bc34cadd58ef947b539b0def8b7a27212
Aarón Valdez <ldaaron38@gmail.com>
2018-09-15 21:51:19 -0600

    started to try some component snapshots

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import { shallow, render, mount } from 'enzyme';
 const Card = props => {
   const { id, name, email } = props;
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img src={`https://robohash.org/${id}?200x200`} alt="Robo app" />
       <div>
         <h2>{name}</h2>
